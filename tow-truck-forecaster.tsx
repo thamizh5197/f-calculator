72cac1f6182127a48b145d212c0ac50f5d71817f
thamizh5197 <tselvas.123@gmail.com>
2026-01-09 22:31:26 +0530

    fix

--- tow-truck-forecaster.tsx
+++ tow-truck-forecaster.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, Bar } from 'recharts';
 
 export default function TowTruckForecaster() {
